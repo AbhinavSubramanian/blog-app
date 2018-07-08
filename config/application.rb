4e8a83d6e1d87d8270642f2998f4adb8f6a2b2a7
Abhinav K Subramanian <abhinavksubramanian@Abhinavs-Air.domain.name>
2018-07-08 22:37:13 +0530

    added rack cors

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Usingjquery
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://abhi-react-app.herokuapp.com/'
+        origins 'https://abhi-react-app.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
